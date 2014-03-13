f1a2d7637469622913842b706ba652635bf310f9
Josue Montano <josuemontanoa@gmail.com>
2014-03-13 09:53:28 -0700

    Update BookController.java

--- No XML Configuration/src/main/java/com/hanovit/app/book/BookController.java
+++ No XML Configuration/src/main/java/com/hanovit/app/book/BookController.java
@@ -3,12 +3,16 @@ package com.hanovit.app.book;
 import com.hanovit.app.book.entities.Book;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 /**
  *
@@ -22,13 +26,16 @@ public class BookController {
     BookService bookService;
     
     @RequestMapping(method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(HttpStatus.OK)
     public @ResponseBody List<Book> list() {
         return this.bookService.findAll();
     }
     
     @RequestMapping(value= "/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-    public @ResponseBody Book find(@RequestParam("id") String title) {
-        return this.bookService.find(title);
+    public ResponseEntity<Book> find(@PathVariable("id") String title) {
+        Book book = this.bookService.find(title);
+        if (book == null) return new ResponseEntity<Book>(HttpStatus.NOT_FOUND);
+        else              return new ResponseEntity<Book>(book, HttpStatus.OK);
     }
     
 }
