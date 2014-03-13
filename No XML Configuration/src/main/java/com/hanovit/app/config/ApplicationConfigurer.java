b2a150df451d2f71206d91a011c371a0cb5fb588
Josue Montano <josuemontanoa@gmail.com>
2014-03-13 09:42:50 -0700

    Update ApplicationConfigurer.java

--- No XML Configuration/src/main/java/com/hanovit/app/config/ApplicationConfigurer.java
+++ No XML Configuration/src/main/java/com/hanovit/app/config/ApplicationConfigurer.java
@@ -3,6 +3,7 @@ package com.hanovit.app.config;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
@@ -11,9 +12,10 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
  * @author josuemontano
  */
 @Configuration
-@ComponentScan({ "com.hanovit.app" })
+@ComponentScan("com.hanovit.app.config")
 @EnableWebMvc
-@Import({ HibernateConfig.class })
+@EnableTransactionManagement
+@Import({HibernateConfig.class})
 public class ApplicationConfigurer extends WebMvcConfigurerAdapter {
     
 }
