8fffb0a594486a861a71441ce52abeeec595b857
Josue Montano <josuemontanoa@gmail.com>
2014-07-08 12:18:20 -0400

    Update BookController.java

--- No XML Configuration/src/main/java/com/hanovit/app/book/BookController.java
+++ No XML Configuration/src/main/java/com/hanovit/app/book/BookController.java
@@ -1,19 +1,14 @@
 package com.hanovit.app.book;
 
 import com.hanovit.app.book.entities.Book;
-import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 /**
  *
@@ -33,25 +28,24 @@ public class BookController {
     }
     
     @RequestMapping(value= "/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<Book> find(@PathVariable("id") String title) {
-        Book book = this.bookService.find(title);
-        if (book == null) return new ResponseEntity<Book>(HttpStatus.NOT_FOUND);
-        else              return new ResponseEntity<Book>(book, HttpStatus.OK);
+    public ResponseEntity<Book> find(@PathVariable("id") int id) {
+        Book book = this.bookService.find(id);
+        if (book == null) return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        else              return new ResponseEntity<>(book, HttpStatus.OK);
     }
     
     @RequestMapping(value = "/{id}", method = RequestMethod.PUT)
-    public ResponseEntity<Book> update(@PathVariable("id") String title, @RequestBody Book book) {
+    public ResponseEntity<Book> update(@PathVariable("id") int id, @RequestBody Book book) {
         //book.setTitle(title);
         Book uBook = this.bookService.save(book);
-        if (uBook == null) return new ResponseEntity<Book>(HttpStatus.NOT_FOUND);
-        return new ResponseEntity<Book>(HttpStatus.OK);
-        //return new ResponseEntity<Book>(HttpStatus.FORBIDDEN);
+        if (uBook == null) return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        else               return new ResponseEntity<>(HttpStatus.OK);
     }
     
     @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
-    public ResponseEntity<Book> delete(@PathVariable("id") String title) {
-        if (this.bookService.delete(title)) return new ResponseEntity<Book>(HttpStatus.OK);
-        return new ResponseEntity<Book>(HttpStatus.FORBIDDEN);
+    public ResponseEntity<Book> delete(@PathVariable("id") int id) {
+        if (this.bookService.delete(id)) return new ResponseEntity<>(HttpStatus.OK);
+        else                             return new ResponseEntity<>(HttpStatus.FORBIDDEN);
     }
     
 }
