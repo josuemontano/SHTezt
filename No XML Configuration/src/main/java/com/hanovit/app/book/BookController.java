a31d3d54f188daa4ec0a8b48af1b93eb1bff92b1
Josue Montano <josuemontanoa@gmail.com>
2014-07-08 10:34:11 -0400

    Update BookController.java

--- No XML Configuration/src/main/java/com/hanovit/app/book/BookController.java
+++ No XML Configuration/src/main/java/com/hanovit/app/book/BookController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,7 +17,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 
 /**
  *
- * @author josuemontano
+ * @author Josue Montano
  */
 @Controller
 @RequestMapping("/books")
